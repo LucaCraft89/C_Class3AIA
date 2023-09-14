65f9ccceaafcf1fc8488bf2aad67a9d3e14800c0
lucac <luca'7h2o@gmail.com>
2023-09-14 11:26:46 +0200

    icao

--- test.cpp
+++ test.cpp
@@ -1,3 +1,28 @@
 //
 // Created by lucac on 9/14/2023.
 //
+#include <iostream>
+
+using namespace std;
+
+class Human {
+private:
+    class Brain { ;
+    public:
+        void Think() {
+            cout << "blyat";
+        }
+    };
+public:
+    int age;
+    float wight;
+    Brain humanbrain;
+};
+
+int main() {
+    Human o;
+    o.humanbrain;
+    o.humanbrain.Think();
+    return 0;
+}
+
